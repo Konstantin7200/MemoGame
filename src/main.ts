3da6e455709572b8a41bcc78751f5f1b468d208c
Konstantin Sl <kostatop2@gmail.com>
2026-01-28 19:40:11 +0300

    Added map creation,basic playing

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 
-
+const counterEl=document.getElementById("counter") as HTMLElement
 const cardContainer=document.getElementById("cardContainer") as HTMLDivElement;
 let n=4;
+let pickedCard:HTMLDivElement|null=null
+let counter=0
 
 createMap(n)
 
 function createMap(n:number){
   let arr=new Array(n*n/2)
+  let cardStorage=new Map<HTMLDivElement,HTMLParagraphElement>()
   for(let i=0;i<arr.length;i++)
   arr[i]=(i+1)*10+2
   for(let i=0;i<n;i++)
@@ -26,18 +29,37 @@ function createMap(n:number){
       else arr[index]--;
 
       cardText.innerText=`${Math.floor(num/10)}`
+
+
       card.addEventListener('click',()=>{
         if(card.classList.contains("visible"))
-        {
-          card.classList.remove("visible")
-          card.removeChild(cardText)
-        }
+          return;
         else{
           card.classList.add("visible")
           card.appendChild(cardText)
-        }  
+          if(pickedCard===null)
+            pickedCard=card
+          else{
+            if(cardStorage.get(pickedCard)!.innerText===cardText.innerText)
+            {
+              counter++;
+              counterEl.innerText=`${counter} cards found`
+            }
+            else{
+              let temp=pickedCard
+              setTimeout(()=>{
+                card.classList.remove("visible")
+                temp.classList.remove("visible")
+                temp.removeChild(cardStorage.get(temp)!)
+                card.removeChild(cardText)
+              },500)
+            }
+            pickedCard=null;
+          }
+        }
       })
-    cardContainer.appendChild(card);
+      cardStorage.set(card,cardText);
+      cardContainer.appendChild(card);
   }
 }
 
